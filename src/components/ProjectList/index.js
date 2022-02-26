077b25bb3544a72b7d922e87db7d241f2bb20dd0
hpurring <hilarypurrington@gmail.com>
2022-02-25 19:32:44 -0500

    card template edit

--- src/components/ProjectList/index.js
+++ src/components/ProjectList/index.js
@@ -2,38 +2,25 @@ import React, { useState } from 'react';
 import Modal from '../Modal';
 
 function ProjectList() {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [projects] = useState([
+  const projects = [
     {
       name: 'HikeSpot',
-      description: 'Find your next hiking location! See the weather, access a new playlist, and plan your escape.',
-      tech: 'HTML, CSS, Bootstrap, server APIs,',
+      description: 'HikeSpot uses HTML, CSS, Bootstrap, and server API calls to help you find your next hiking location. View the current weather, access a new playlist, and plan your escape.',
       image: '../assets/projects/hikespot.png',
-      github: 'https://github.com/hpurring/proj-01-HikeSpot',
-      deployed: 'https://hpurring.github.io/proj-01-HikeSpot/'
+      demo: 'https://hpurring.github.io/proj-01-HikeSpot/',
+      github: 'https://github.com/hpurring/proj-01-HikeSpot'
     },
     {
       name: 'Blech',
-      description: 'a tech blog where users can create blog posts and comment on others\' posts.',
-      tech: 'HTML, CSS, Handlebars, Express.js, MySQL, Sequelize',
+      description: 'Built with HTML, CSS, Handlebars, Express.js, MySQL, and Sequelize, blech! allows users to create blog posts and comment on others\' posts.',
       image: '../assets/projects/blech.png',
-      github: 'https://github.com/hpurring/blech',
-      deployed: 'https://still-citadel-57293.herokuapp.com/'
+      demo: 'https://still-citadel-57293.herokuapp.com/',
+      github: 'https://github.com/hpurring/blech'
     }
-  ]);
-
-  const [currentProject, setCurrentProject] = useState();
-
-  const toggleModal = (image, i) => {
-    setCurrentProject({...image, index: i});
-    setIsModalOpen(!isModalOpen);
-  };
+  ];
 
   return (
     <div>
-      {isModalOpen && (
-      <Modal onClose={toggleModal} currentProject={currentProject} />
-      )}
       <div className="flex-row">
         {currentProject.map((projects, i) => (
           <img
