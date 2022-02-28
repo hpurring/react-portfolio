a6584dbb3e68c20255a5e06fae990831a097d809
hpurring <hilarypurrington@gmail.com>
2022-02-28 12:58:49 -0500

    footer images

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,15 +4,15 @@ import './footer.css'
 function Footer() {
     return (
         <section id='footer'>
-            <div className='home-buttons'>
-                <a className='home-btn' href="https://github.com/hpurring" target="_blank">github</a>
-                <a className='home-btn' href="https://www.linkedin.com/in/hilarypurrington/" target="_blank">linkedin</a>
-                <a className='home-btn' href="mailto:hilarypurrington@gmail.com" target="_blank">email</a>
-                <a className='home-btn' href="https://www.instagram.com/hilarypurrington/" target="_blank">instagram</a>
+            <div className='home-buttons'>    
+                <a className='home-btn' href="https://github.com/hpurring" target="_blank"><img src={require("../../assets/photos/github.png")} alt="Github link" /></a>
+                <a className='home-btn' href="https://www.linkedin.com/in/hilarypurrington/" target="_blank"><img src={require("../../assets/photos/linkedin.svg.png")} alt="LinkedIn link"/></a>
+                <a className='home-btn' href="mailto:hilarypurrington@gmail.com" target="_blank"><img src={require("../../assets/photos/envelope.svg.png")} alt="email link"/></a>
+                <a className='home-btn' href="https://www.instagram.com/hilarypurrington/" target="_blank"><img src={require("../../assets/photos/instagram.png")} alt="Instagram link"/></a>
             </div>
-            <div id='copyright'>
+            {/* <div id='copyright'>
             ©2022 by Hilary Purrington. All rights reserved.
-            </div>
+            </div> */}
         </section>
 
     );
