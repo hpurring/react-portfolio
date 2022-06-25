ab8b04649b67be515e2a92bab78f859b28658958
hpurring <hilarypurrington@gmail.com>
2022-06-25 15:17:41 -0400

    tech stack

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -7,7 +7,7 @@ function About() {
             <div className='bio-photo'>
                 <div className='title-content'>
                     <h1 id='about'>about me</h1>
-                    <a className='bio-content'>Hi there! I'm a full-stack developer with a background in classical music and non-profit fundraising. A gifted writer and communicator, I'm studying coding and software development to expand my ability to meaningfully and creatively interact with mission-driven organizations. Originally from western Massachusetts, I lived in New York City for a number of years and recently relocated to Durham, North Carolina. In addition to pursuing my work as a software developer, I continue to freelance as a classical composer specializing in concert music. 
+                    <a className='bio-content'>Hi there! I'm a full-stack developer with a background in classical music and non-profit fundraising. Originally from western Massachusetts, I lived in New York City for a number of years and recently relocated to Durham, North Carolina. In addition to pursuing my work as a software developer, I continue to freelance as a classical composer specializing in concert music. At its core, composing is creative problem solving—not unlike coding. My musical training has meaningfully supported my development as a software engineer. 
                     <br></br>
                     <br></br>
                     I received a bachelor's degree from the Shepherd School of Music at Rice University, master's degrees from The Juilliard School and Yale University, and I'm currently completing a coding certificate at Columbia University's School of Engineering.</a>
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -18,25 +18,83 @@ function Home() {
         <h1 className="bio-header">about me</h1>
         <div className="bio-content">
           <a>
-            Hi there! I'm a full-stack developer with a background in classical
-            music and non-profit fundraising. A gifted writer and communicator,
-            my full-stack software development skills have dramatically enhanced my ability to
-            meaningfully and creatively interact with mission-driven
-            organizations. 
+            Hi there! I'm a full-stack developer with a background in classical music, non-profit fundraising, and communications. Originally from western Massachusetts, I lived in New York City for a number of years before relocating to Durham, North Carolina. 
             <br></br>
             <br></br>
-            Originally from western Massachusetts, I lived in New
-            York City for a number of years and recently relocated to Durham,
-            North Carolina. In addition to pursuing my work as a software
-            developer, I continue to freelance as a classical composer
-            specializing in concert music.
+            In addition to pursuing my work as a software developer, I continue to freelance as a classical composer specializing in concert music. At its core, composing music is creative problem solving—not unlike coding. My musical training has meaningfully informed my development as a software engineer, allowing me to think creatively in both solo and collaborative efforts.
             <br></br>
-            <br></br>I received a bachelor's degree from the Shepherd School of
-            Music at Rice University, master's degrees from The Juilliard School
-            and Yale University, and a coding
-            certificate from Columbia University's School of Engineering.
+            <br></br>I received a bachelor's degree from the Shepherd School of Music at Rice University, master's degrees from The Juilliard School and Yale University, and a certificate in full-stack web development from Columbia University's School of Engineering.
           </a>
         </div>
+      </div>
+      <div className="tech-container">
+      <img 
+        src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-plain-wordmark.svg" 
+        alt="HTML5"
+        className="tech-icon"        
+      />
+      <img 
+        src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original-wordmark.svg"
+        alt="CSS3"
+        className="tech-icon"
+      />
+      <img 
+        src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" 
+        alt="JavaScript"
+        className="tech-icon" 
+      />
+      <img 
+        src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original-wordmark.svg"          
+        alt="React"
+        className="tech-icon" 
+      />
+      <img 
+        src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jquery/jquery-original-wordmark.svg"          
+        alt="JQuery"
+        className="tech-icon" 
+      />
+       <img 
+        src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original-wordmark.svg"                   
+        alt="Node.js"
+        className="tech-icon" 
+      />
+       <img 
+        src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original-wordmark.svg"
+        alt="Express.js"
+        className="tech-icon" 
+      />
+      <img 
+        src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/handlebars/handlebars-original-wordmark.svg"
+        alt="Handlebars"
+        className="tech-icon" 
+      />
+      <img 
+        src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original-wordmark.svg"          
+        alt="MySQL"
+        className="tech-icon" 
+      />
+      <img 
+        src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original-wordmark.svg"         
+        alt="MongoDB"
+        className="tech-icon" 
+      />
+      <img 
+        src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original-wordmark.svg" 
+        alt="GitHub"
+        className="tech-icon" 
+      />
+      <img 
+        src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/heroku/heroku-plain-wordmark.svg"
+        alt="Heroku"
+        className="tech-icon" 
+      />
+      
+
+
+
+
+
+
       </div>
     </div>
   );
